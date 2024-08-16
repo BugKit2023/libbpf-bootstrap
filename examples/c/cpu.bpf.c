27e95526587e1a60417e0844e6cd5c6dc728d1c0
Daria <daria.yurchenko@sigma.software>
2024-08-16 18:19:31 +0200

    Add cpu

--- examples/c/cpu.bpf.c
+++ examples/c/cpu.bpf.c
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
-/* Copyright (c) 2020 Facebook */
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
@@ -8,9 +7,10 @@
 struct {
     __uint(type, BPF_MAP_TYPE_HASH);
     __uint(max_entries, 1024);
-    __type(key, u32);   
+    __type(key, u32);
     __type(value, u64);
 } cpu_times SEC(".maps");
+
 struct {
     __uint(type, BPF_MAP_TYPE_HASH);
     __uint(max_entries, 1024);
@@ -23,21 +23,24 @@ char LICENSE[] SEC("license") = "Dual BSD/GPL";
 SEC("tracepoint/sched/sched_switch")
 int on_sched_switch(struct pt_regs *ctx) {
     u32 pid = bpf_get_current_pid_tgid() >> 32;
-    u64 ts = bpf_ktime_get_ns(); 
+    u64 ts = bpf_ktime_get_ns();
 
     u64 *last_ts = bpf_map_lookup_elem(&last_update, &pid);
     u64 *prev_cpu_time = bpf_map_lookup_elem(&cpu_times, &pid);
 
     if (last_ts && prev_cpu_time) {
         u64 delta = ts - *last_ts;
-        u64 updated_time = delta + *prev_cpu_time;
+        u64 updated_time = *prev_cpu_time + delta;
 
+        // Update the CPU time for the process
         bpf_map_update_elem(&cpu_times, &pid, &updated_time, BPF_ANY);
+        // Update the last timestamp for the process
         bpf_map_update_elem(&last_update, &pid, &ts, BPF_ANY);
     } else {
-        bpf_map_update_elem(&last_update, &pid, &ts, BPF_ANY);
+        // If no previous data, initialize
         u64 initial_cpu_time = 0;
         bpf_map_update_elem(&cpu_times, &pid, &initial_cpu_time, BPF_ANY);
+        bpf_map_update_elem(&last_update, &pid, &ts, BPF_ANY);
     }
 
     return 0;
--- examples/c/cpu.c
+++ examples/c/cpu.c
@@ -18,49 +18,56 @@ static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va
 
 int main(int argc, char **argv)
 {
-	struct cpu_bpf *skel;
-	int err;
+    struct cpu_bpf *skel;
+    int err;
 
-	/* Set up libbpf errors and debug info callback */
-	libbpf_set_print(libbpf_print_fn);
+    /* Set up libbpf errors and debug info callback */
+    libbpf_set_print(libbpf_print_fn);
 
-	/* Open BPF application */
-	skel = cpu_bpf__open();
-	if (!skel) {
-		fprintf(stderr, "Failed to open BPF skeleton\n");
-		return 1;
-	}
+    /* Open BPF application */
+    skel = cpu_bpf__open();
+    if (!skel) {
+        fprintf(stderr, "Failed to open BPF skeleton\n");
+        return 1;
+    }
 
-	/* Load & verify BPF programs */
-	err = cpu_bpf__load(skel);
-	if (err) {
-		fprintf(stderr, "Failed to load and verify BPF skeleton\n");
-		goto cleanup;
-	}
+    /* Load & verify BPF programs */
+    err = cpu_bpf__load(skel);
+    if (err) {
+        fprintf(stderr, "Failed to load and verify BPF skeleton\n");
+        goto cleanup;
+    }
 
-	/* Attach tracepoint handler */
-	err = cpu_bpf__attach(skel);
-	if (err) {
-		fprintf(stderr, "Failed to attach BPF skeleton\n");
-		goto cleanup;
-	}
+    /* Attach tracepoint handler */
+    err = cpu_bpf__attach(skel);
+    if (err) {
+        fprintf(stderr, "Failed to attach BPF skeleton\n");
+        goto cleanup;
+    }
 
-	printf("Tracking CPU usage. Press Ctrl+C to stop.\n");
+    printf("Tracking CPU usage. Press Ctrl+C to stop.\n");
 
     while (1) {
-         __u32 pid = 0, next_pid;
-         __u64 current_value, previous_value;
+        __u32 pid = 0, next_pid;
+        __u64 current_value, previous_value;
 
-         while (bpf_map_get_next_key(bpf_map__fd(skel->maps.cpu_times), &pid, &next_pid) == 0) {
-             if (bpf_map_lookup_elem(bpf_map__fd(skel->maps.cpu_times), &next_pid, &current_value) == 0) {
-                 __u64 delta_ns = current_value - previous_value;
-                 __u64 delta_mcpu = (delta_ns * 1000) / (TIME_WINDOW_SEC * SEC_TO_NS);
-                 printf("Process %u used %llu mCPU in the last 5 seconds\n", next_pid, delta_mcpu);
-                 previous_value = current_value;
-             }
-             pid = next_pid;
-         }
-         sleep(TIME_WINDOW_SEC);
+        while (bpf_map_get_next_key(bpf_map__fd(skel->maps.cpu_times), &pid, &next_pid) == 0) {
+            if (bpf_map_lookup_elem(bpf_map__fd(skel->maps.cpu_times), &next_pid, &current_value) == 0) {
+                if (bpf_map_lookup_elem(bpf_map__fd(skel->maps.prev_cpu_times), &next_pid, &previous_value) != 0) {
+                    previous_value = 0;  // Default previous value if not found
+                }
+
+                __u64 delta_ns = current_value - previous_value;
+                __u64 delta_mcpu = (delta_ns * 1000) / (TIME_WINDOW_SEC * SEC_TO_NS);
+
+                printf("Process %u used %llu mCPU in the last 5 seconds\n", next_pid, delta_mcpu);
+
+                // Update the previous CPU time for the next interval
+                bpf_map_update_elem(bpf_map__fd(skel->maps.prev_cpu_times), &next_pid, &current_value, BPF_ANY);
+            }
+            pid = next_pid;
+        }
+        sleep(TIME_WINDOW_SEC);
     }
 
 cleanup:
