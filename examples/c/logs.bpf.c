a434fa15cb1db36fd0bb48ee6628d0334c07c903
Daria <daria.yurchenko@sigma.software>
2024-08-18 17:14:21 +0200

    Add logs

--- examples/c/logs.bpf.c
+++ examples/c/logs.bpf.c
@@ -9,7 +9,7 @@ char LICENSE[] SEC("license") = "Dual BSD/GPL";
 SEC("tracepoint/syscalls/sys_enter_write")
 int trace_write(void *ctx) {
     int pid = bpf_get_current_pid_tgid() >> 32;
-    bpf_trace_printk("HELLO %d\n", pid);
+    bpf_trace_printk("HELLO %d\n", sizeof("HELLO %d\n"), pid);
 
     return 0;
 }
