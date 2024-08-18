79e2a916e59e5962addbe07e33234dbd3e965d66
Daria <daria.yurchenko@sigma.software>
2024-08-18 15:09:02 +0200

    Add logs

--- examples/c/logs.c
+++ examples/c/logs.c
@@ -6,18 +6,6 @@
 #include <bpf/libbpf.h>
 #include "logs.skel.h"
 
-#define LOG_BUF_SIZE 256
-
-//struct log_event {
-//    int pid;
-//    char data[LOG_BUF_SIZE];
-//};
-
-// Функция для обработки событий из perf buffer
-//static void handle_event(void *ctx, int cpu, void *data, __aligned(8) __size_t size) {
-//    struct log_event *event = (struct log_event *)data;
-//    printf("Captured log from PID %d: %s\n", event->pid, event->data);
-//}
 
 static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args) {
     return vfprintf(stderr, format, args);
@@ -25,8 +13,6 @@ static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va
 
 int main(int argc, char **argv) {
     struct logs_bpf *skel;
-//    struct logs_map *map;
-//    int map_fd;
     int err;
 
     /* Set up libbpf errors and debug info callback */
@@ -46,14 +32,6 @@ int main(int argc, char **argv) {
         goto cleanup;
     }
 
-//    map = bpf_object__find_map_by_name(obj, "events");
-//    if (!map) {
-//        fprintf(stderr, "Failed to find map\n");
-//        return 1;
-//    }
-//
-//    map_fd = logs_map__fd(map);
-
     /* Attach tracepoint handler */
     err = logs_bpf__attach(skel);
     if (err) {
@@ -61,24 +39,7 @@ int main(int argc, char **argv) {
         goto cleanup;
     }
 
-//    struct bpf_ring_buffer *rb;
-//    rb = bpf_ring_buffer__new(map_fd, handle_event, NULL, NULL);
-//    if (!rb) {
-//        perror("bpf_ring_buffer__new");
-//        return 1;
-//    }
-//
-//    printf("Listening for log events. Press Ctrl+C to stop.\n");
-//
-//    // Основной цикл обработки событий
-//    while (1) {
-//        if (bpf_ring_buffer__poll(rb, 100) < 0) {
-//            perror("bpf_ring_buffer__poll");
-//        }
-//    }
-//
-//    bpf_ring_buffer__free(rb);
-    return 0;
+    printf("Tracking Logs usage. Press Ctrl+C to stop.\n");
 
 cleanup:
     logs_bpf__destroy(skel);
