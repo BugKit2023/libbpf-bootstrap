85ed7901d9666b91d549a425d95dca3c81554c6b
Daria <daria.yurchenko@sigma.software>
2024-08-18 18:36:11 +0200

    Add logs

--- examples/c/logs.bpf.c
+++ examples/c/logs.bpf.c
@@ -4,12 +4,20 @@
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_core_read.h>
 
+#define STDOUT_FD 1
+
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
 SEC("tracepoint/syscalls/sys_enter_write")
 int trace_write(void *ctx) {
-    int pid = bpf_get_current_pid_tgid() >> 32;
-    bpf_trace_printk("HELLO %d\n", sizeof("HELLO %d\n"), pid);
+    int fd = BPF_CORE_READ(ctx, fd);
+    const char *buf = (const char *)BPF_CORE_READ(ctx, buf);
+    size_t count = BPF_CORE_READ(ctx, count);
+
+    if (fd == STDOUT_FD) {
+        int pid = bpf_get_current_pid_tgid() >> 32;
+        bpf_trace_printk("HELLO %d\n", sizeof("HELLO %d\n"), pid);
+    }
 
     return 0;
 }
