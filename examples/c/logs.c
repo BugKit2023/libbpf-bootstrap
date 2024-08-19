605bfc75fc92a3c9c1731b74ddac8639beea14bf
Daria <daria.yurchenko@sigma.software>
2024-08-19 22:51:34 +0200

    Add logs

--- examples/c/logs.c
+++ examples/c/logs.c
@@ -56,15 +56,15 @@ int main(int argc, char **argv) {
 
     while (1) {
         printf("HELLO");
-//        ssize_t bytes = read(perf_fd, buf, sizeof(buf));
-//        if (bytes < 0) {
-//            perror("read failed");
-//            break;
-//        }
+        ssize_t bytes = read(perf_fd, buf, sizeof(buf));
+        if (bytes <= 0) {
+            print("read failed");
+            continue;
+        }
 //
 //        /* Process the data in `buf` */
 //        char *ptr = buf;
-//        sleep(2);
+        sleep(2);
     }
 
     close(perf_fd);
