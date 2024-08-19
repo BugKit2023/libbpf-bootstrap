faf00f6909249911e7ee6137f793cfa403b75d9e
Daria <daria.yurchenko@sigma.software>
2024-08-19 21:26:44 +0200

    Add logs

--- examples/c/logs.c
+++ examples/c/logs.c
@@ -7,6 +7,7 @@
 #include "logs.skel.h"
 
 #define BUF_SIZE 4096
+#define POLL_TIMEOUT 5000
 
 static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args) {
     return vfprintf(stderr, format, args);
@@ -16,6 +17,7 @@ int main(int argc, char **argv) {
     struct logs_bpf *skel;
     int err;
     int perf_fd;
+    struct pollfd pfd;
     char buf[BUF_SIZE];
 
     /* Set up libbpf errors and debug info callback */
@@ -49,18 +51,38 @@ int main(int argc, char **argv) {
     }
 
     printf("Tracking Logs usage. Press Ctrl+C to stop.\n");
+    printf("Attempting to read from perf_fd: %d\n", perf_fd);
+
+    pfd.fd = perf_fd;
+    pfd.events = POLLIN;  // Мониторинг на доступность данных для чтения
 
     while (1) {
-        printf("HELLO");
-        ssize_t bytes = read(perf_fd, buf, sizeof(buf));
-        if (bytes < 0) {
-            perror("read failed");
+        // Ожидаем, пока появятся данные
+        int ret = poll(&pfd, 1, POLL_TIMEOUT); // Тайм-аут 5000 миллисекунд
+
+        if (ret < 0) {
+            perror("poll failed");
             break;
+        } else if (ret == 0) {
+            printf("Timeout, no data available\n");
+            continue;
         }
 
-        /* Process the data in `buf` */
-        char *ptr = buf;
-        sleep(2);
+        // Если есть доступные данные для чтения
+        if (pfd.revents & POLLIN) {
+            ssize_t bytes = read(perf_fd, buf, sizeof(buf));
+            if (bytes < 0) {
+                perror("read failed");
+                break;
+            } else if (bytes == 0) {
+                printf("No data read\n");
+            } else {
+                // Обработка данных из `buf`
+                printf("Data received: %zd bytes\n", bytes);
+            }
+        }
+        printf("HELLO!");
+        sleep(5);
     }
 
     close(perf_fd);
