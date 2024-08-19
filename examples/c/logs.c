5664eb800b13e8cd5364127ea9f55e2a2a32ca1a
Daria <daria.yurchenko@sigma.software>
2024-08-19 20:34:36 +0200

    Add logs

--- examples/c/logs.c
+++ examples/c/logs.c
@@ -6,6 +6,21 @@
 #include <bpf/libbpf.h>
 #include "logs.skel.h"
 
+struct Event {
+    uint64_t timestamp;
+    int fd;
+    int pid;
+};
+
+void process_event(void *data, size_t size) {
+    // Предполагаем, что структура Event соответствует размеру данных
+    struct Event *event = (struct Event *)data;
+
+    printf("Timestamp: %llu\n", event->timestamp);
+    printf("File Descriptor: %d\n", event->fd);
+    printf("PID: %d\n", event->pid);
+}
+
 
 static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args) {
     return vfprintf(stderr, format, args);
@@ -17,6 +32,21 @@ int main(int argc, char **argv) {
 
     /* Set up libbpf errors and debug info callback */
     libbpf_set_print(libbpf_print_fn);
+
+    struct perf_event_attr pe = {
+        .type = PERF_TYPE_SOFTWARE,
+        .config = PERF_COUNT_SW_BPF_OUTPUT,
+        .size = sizeof(pe),
+        .disabled = 1,
+        .exclude_kernel = 1,
+        .exclude_hv = 1,
+    }
+
+    int perf_fd = perf_event_open(&pe, -1, -1, -1, 0);
+    if (perf_fd < 0) {
+        perror("perf_event_open failed");
+        return 1;
+    }
 
     /* Open BPF application */
     skel = logs_bpf__open();
@@ -41,9 +71,14 @@ int main(int argc, char **argv) {
 
     printf("Tracking Logs usage. Press Ctrl+C to stop.\n");
 
+    char buf[4096];
     while (1) {
+        ssize_t bytes = read(perf_fd, buf, sizeof(buf));
+        printf("Hello")
         sleep(5);
     }
+
+    close(perf_fd);
 
 cleanup:
     logs_bpf__destroy(skel);
