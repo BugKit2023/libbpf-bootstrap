193edd252596527b1873a107b421661ba8408052
Daria <daria.yurchenko@sigma.software>
2024-08-25 21:46:36 +0200

    Add traces

--- examples/c/traces.bpf.c
+++ examples/c/traces.bpf.c
@@ -100,6 +100,37 @@ int kprobe_tcp_sendmsg(struct pt_regs *ctx) {
     return 0;
 }
 
+SEC("tracepoint/syscalls/sys_enter_recvfrom")
+int trace_sendto(struct trace_event_raw_sys_enter* ctx) {
+    struct trace_event_t event = {};
+
+    void *buf;
+    unsigned int buf_size;
+    char data[MAX_DATA_SIZE] = {};
+
+    buf = (void *)ctx->args[1];
+    buf_size = (unsigned int) ctx->args[
+    if (buf_size < 0) {
+        return 0;
+    }
+    // Ограничиваем размер буфера, чтобы избежать переполнения
+    if (buf_size > MAX_DATA_SIZE) {
+        buf_size = MAX_DATA_SIZE;
+
+    // Считываем данные из пользовательского пространства
+    bpf_probe_read_user(&event.data, buf_size, bu
+    event.pid = bpf_get_current_pid_tgid() >> 32;
+    event.tid = bpf_get_current_pid_tgid();
+    event.type = 3;
+    event.start_ts = bpf_ktime_get_ns();
+    //__builtin_memcpy(event.data, data, buf_siz
+    // Логируем размер и содержимое данных
+    bpf_printk("curl sys_enter_recvfrom() called: data_len=%d, data=%s\n", buf_size, event.data);
+    bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &event, sizeof(event));
+
+    return 0;
+}
+
 SEC("kprobe/tcp_recvmsg")
 int trace_tcp_recvmsg(struct pt_regs *ctx) {
     struct trace_event_t event = {};
