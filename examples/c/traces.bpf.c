b7c43e096817205778fe9556b37bbfe2f47747d9
Daria <daria.yurchenko@sigma.software>
2024-08-25 18:27:21 +0200

    Add traces

--- examples/c/traces.bpf.c
+++ examples/c/traces.bpf.c
@@ -7,16 +7,10 @@
 #define MAX_BUF_SIZE 64
 
 struct trace_event_t {
-    __u32 pid;
-    __u32 tid;
     __u32 saddr;
     __u32 daddr;
     __u16 sport;
     __u16 dport;
-    __u64 start_ts;
-    __u64 end_ts;
-    __u32 http_method;
-    __u32 status_code;
     char uri[MAX_BUF_SIZE];
 };
 
@@ -39,18 +33,22 @@ int socket_handler(struct __sk_buff *skb) {
     __u32 payload_offset, payload_length;
     char line_buffer[7];
 
+    // Load Ethernet protocol
     bpf_skb_load_bytes(skb, 12, &proto, sizeof(proto));
     proto = bpf_ntohs(proto);
     if (proto != 0x0800) // ETH_P_IP
         return 0;
 
+    // Load IP header length
     bpf_skb_load_bytes(skb, nhoff, &hdr_len, sizeof(hdr_len));
     hdr_len = (hdr_len & 0x0F) * 4;
 
+    // Load protocol type
     bpf_skb_load_bytes(skb, nhoff + offsetof(struct iphdr, protocol), &ip_proto, sizeof(ip_proto));
     if (ip_proto != IPPROTO_TCP)
         return 0;
 
+    // Calculate TCP header length
     tcp_hdr_len = nhoff + hdr_len;
     bpf_skb_load_bytes(skb, nhoff + offsetof(struct iphdr, tot_len), &tlen, sizeof(tlen));
     tlen = bpf_ntohs(tlen);
@@ -69,25 +67,20 @@ int socket_handler(struct __sk_buff *skb) {
     bpf_skb_load_bytes(skb, payload_offset, line_buffer, sizeof(line_buffer));
 
     if (line_buffer[0] == 'G' && line_buffer[1] == 'E' && line_buffer[2] == 'T') {
-        event.http_method = 1;
+        bpf_skb_load_bytes(skb, payload_offset + 4, event.uri, MAX_BUF_SIZE);
     } else if (line_buffer[0] == 'P' && line_buffer[1] == 'O' && line_buffer[2] == 'S' && line_buffer[3] == 'T') {
-        event.http_method = 2;
+        bpf_skb_load_bytes(skb, payload_offset + 5, event.uri, MAX_BUF_SIZE);
     } else {
         return 0;
     }
 
-    struct task_struct *task = (struct task_struct *)bpf_get_current_task();
-    event.pid = BPF_CORE_READ(task, pid);
-    event.tid = bpf_get_current_pid_tgid();
-    event.start_ts = bpf_ktime_get_ns();
-
+    // Load IP addresses and ports
     bpf_skb_load_bytes(skb, nhoff + offsetof(struct iphdr, saddr), &event.saddr, sizeof(event.saddr));
     bpf_skb_load_bytes(skb, nhoff + offsetof(struct iphdr, daddr), &event.daddr, sizeof(event.daddr));
     bpf_skb_load_bytes(skb, tcp_hdr_len + offsetof(struct tcphdr, source), &event.sport, sizeof(event.sport));
     bpf_skb_load_bytes(skb, tcp_hdr_len + offsetof(struct tcphdr, dest), &event.dport, sizeof(event.dport));
 
-    bpf_skb_load_bytes(skb, payload_offset + 4, event.uri, MAX_BUF_SIZE);
-
+    // Submit event
     bpf_perf_event_output(skb, &events, BPF_F_CURRENT_CPU, &event, sizeof(event));
     return 0;
 }
