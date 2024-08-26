583cb16f6d4adb42f74bdc228ef3a71975f6625e
Daria <daria.yurchenko@sigma.software>
2024-08-26 13:11:15 +0200

    Add traces

--- examples/c/traces.bpf.c
+++ examples/c/traces.bpf.c
@@ -34,8 +34,6 @@ char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
 SEC("tracepoint/syscalls/sys_enter_sendto")
 int trace_sendto(struct trace_event_raw_sys_enter* ctx) {
-    char comm[TASK_COMM_LEN];
-
     struct trace_event_t event = {};
 
     void *buf;
@@ -43,11 +41,37 @@ int trace_sendto(struct trace_event_raw_sys_enter* ctx) {
     char data[MAX_DATA_SIZE] = {};
 
     // Получаем имя текущего процесса
-    bpf_get_current_comm(&comm, sizeof(comm));
+    //bpf_get_current_comm(&comm, sizeof(comm));
 
     // Проверяем, что процесс называется "curl"
-    if (comm[0] == 'c' && comm[1] == 'u' && comm[2] == 'r' && comm[3] == 'l' && comm[4] == '\0') {
-        // Извлекаем указатель на буфер данных и его размер
+//    if (comm[0] == 'c' && comm[1] == 'u' && comm[2] == 'r' && comm[3] == 'l' && comm[4] == '\0') {
+//        // Извлекаем указатель на буфер данных и его размер
+//        buf = (void *)ctx->args[1];
+//        buf_size = (unsigned int) ctx->args[2];
+//
+//        if (buf_size < 0) {
+//            return 0;
+//        }
+//        // Ограничиваем размер буфера, чтобы избежать переполнения
+//        if (buf_size > MAX_DATA_SIZE) {
+//            buf_size = MAX_DATA_SIZE;
+//        }
+//
+//        // Считываем данные из пользовательского пространства
+//        bpf_probe_read_user(&event.data, buf_size, buf);
+//
+//        event.pid = bpf_get_current_pid_tgid() >> 32;
+//        event.tid = bpf_get_current_pid_tgid();
+//        event.type = 1;
+//        event.start_ts = bpf_ktime_get_ns();
+//        //__builtin_memcpy(event.data, data, buf_size);
+//
+//        // Логируем размер и содержимое данных
+//        bpf_printk("curl sendto() called: data_len=%d, data=%s\n", buf_size, event.data);
+//        bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &event, sizeof(event));
+//    }
+
+    if (strstr(event.data, "HTTP") != NULL) {
         buf = (void *)ctx->args[1];
         buf_size = (unsigned int) ctx->args[2];
 
@@ -82,9 +106,9 @@ int kprobe_tcp_sendmsg(struct pt_regs *ctx) {
     char comm[16];
     bpf_get_current_comm(&comm, sizeof(comm));
 
-    if (comm[0] != 'c' || comm[1] != 'u' || comm[2] != 'r' || comm[3] != 'l' || comm[4] != '\0') {
-        return 0;
-    }
+//    if (comm[0] != 'c' || comm[1] != 'u' || comm[2] != 'r' || comm[3] != 'l' || comm[4] != '\0') {
+//        return 0;
+//    }
 
     event.pid = bpf_get_current_pid_tgid() >> 32;
     event.tid = bpf_get_current_pid_tgid();
@@ -99,7 +123,9 @@ int kprobe_tcp_sendmsg(struct pt_regs *ctx) {
     event.sport = BPF_CORE_READ(sk, __sk_common.skc_num);
     event.dport = BPF_CORE_READ(sk, __sk_common.skc_dport);
 
-    bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &event, sizeof(event));
+    if (event.dport == 8080 || event.dport == 8081 || event.dport == 8082) {
+        bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &event, sizeof(event));
+    }
 
     return 0;
 }
@@ -110,7 +136,7 @@ int trace_tcp_recvmsg(struct pt_regs *ctx) {
 
     event.pid = bpf_get_current_pid_tgid() >> 32;
     event.tid = bpf_get_current_pid_tgid();
-    event.type = 4;
+    event.type = 3;
     event.start_ts = bpf_ktime_get_ns();
 
     struct sock *sk = (struct sock *)PT_REGS_PARM1(ctx);
@@ -121,7 +147,9 @@ int trace_tcp_recvmsg(struct pt_regs *ctx) {
     event.sport = BPF_CORE_READ(sk, __sk_common.skc_num);
     event.dport = BPF_CORE_READ(sk, __sk_common.skc_dport);
 
-    bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &event, sizeof(event));
+    if (event.dport == 8080 || event.dport == 8081 || event.dport == 8082) {
+        bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &event, sizeof(event));
+    }
 
     return 0;
 }
