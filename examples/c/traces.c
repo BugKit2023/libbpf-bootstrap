3a48245e8871f1a66785f55521ef9fe2bfcb80ab
Daria <daria.yurchenko@sigma.software>
2024-08-26 13:56:48 +0200

    Add traces

--- examples/c/traces.c
+++ examples/c/traces.c
@@ -31,8 +31,7 @@ static void print_bpf_output(void *ctx, int cpu, void *data, __u32 size) {
     inet_ntop(AF_INET, &e->saddr, saddr_str, INET_ADDRSTRLEN);
     inet_ntop(AF_INET, &e->daddr, daddr_str, INET_ADDRSTRLEN);
 
-    printf("TYPE: %u\n", e->type);
-    if ((e->type != 1 || strstr(e->data, "HTTP") != NULL) && (ntohs(e->dport) == 8080 || ntohs(e->dport) == 8081 || ntohs(e->dport) == 8082)) {
+    if (e->type != 1 && (ntohs(e->dport) == 8080 || ntohs(e->dport) == 8081 || ntohs(e->dport) == 8082)) || strstr(e->data, "HTTP") != NULL) {
         printf("PID: %u\n", e->pid);
         printf("TID: %u\n", e->tid);
         printf("Source IP: %s\n", saddr_str);
